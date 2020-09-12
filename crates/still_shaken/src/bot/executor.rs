cb4c00ef18db6ad83f4fe04d6dbd7309aa63c3b6
museun <museun@outlook.com>
2020-09-12 19:17:51 -0400

    note that the executor cannot be explicitly shutdown

--- crates/still_shaken/src/bot/executor.rs
+++ crates/still_shaken/src/bot/executor.rs
@@ -1,3 +1,4 @@
+use futures_lite::future;
 use std::{future::Future, sync::Arc};
 
 #[derive(Clone)]
@@ -6,27 +7,28 @@ pub struct Executor {
 }
 
 impl Executor {
+    /// TODO allow this to shutdown via a channel
     pub fn new(threads: usize) -> Self {
-        let ex = Arc::new(async_executor::Executor::new());
+        let inner = Arc::new(async_executor::Executor::new());
 
         for i in 1..=threads {
             std::thread::Builder::new()
                 .name(format!("still_shaken-{}", i))
                 .spawn({
-                    let ex = Arc::clone(&ex);
+                    let ex = Arc::clone(&inner);
                     log::debug!("spawning executor thread");
                     move || loop {
                         let _ = std::panic::catch_unwind(|| {
-                            async_io::block_on(ex.run(futures_lite::future::pending::<()>()))
+                            let fut = ex.run(future::pending::<()>());
+                            future::block_on(fut)
                         });
-
                         log::debug!("end of executor thread ({})", i);
                     }
                 })
                 .expect("named thread support");
         }
 
-        Self { inner: ex }
+        Self { inner }
     }
 }
 
