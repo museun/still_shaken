0c3e9b7fbbe7f0bb4453f34b7be868fa0a4789a3
museun <museun@outlook.com>
2020-09-14 19:35:45 -0400

    add user agent to http requests

--- crates/still_shaken/src/http.rs
+++ crates/still_shaken/src/http.rs
@@ -1,6 +1,15 @@
 #![cfg_attr(debug_assertions, allow(dead_code))]
 use serde::{Deserialize, Serialize};
 
+const USER_AGENT: &str = concat!(
+    env!("CARGO_PKG_NAME"),
+    "/",
+    env!("CARGO_PKG_VERSION"),
+    // " (",
+    // env!("CARGO_PKG_REPOSITORY"),
+    // ")"
+);
+
 pub async fn get_json_with_body<T, E>(ep: &str, body: E) -> anyhow::Result<T>
 where
     for<'de> T: Deserialize<'de> + Send + Sync + 'static,
@@ -25,6 +34,7 @@ where
 {
     attohttpc::get(ep)
         .json(&body)?
+        .header("User-Agent", USER_AGENT)
         .send()?
         .json()
         .map_err(Into::into)
@@ -34,5 +44,9 @@ pub fn sync_get_json<T>(ep: &str) -> anyhow::Result<T>
 where
     for<'de> T: Deserialize<'de> + Send + Sync + 'static,
 {
-    attohttpc::get(ep).send()?.json().map_err(Into::into)
+    attohttpc::get(ep)
+        .header("User-Agent", USER_AGENT)
+        .send()?
+        .json()
+        .map_err(Into::into)
 }
