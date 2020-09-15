59e0a0b11d75500775dfac8507935644d5121626
museun <museun@outlook.com>
2020-09-14 21:22:05 -0400

    add test for uptime

--- crates/still_shaken/src/modules/uptime.rs
+++ crates/still_shaken/src/modules/uptime.rs
@@ -1,8 +1,12 @@
-use std::sync::Arc;
+#[cfg(test)]
+use mock_instant::Instant;
+#[cfg(not(test))]
 use std::time::Instant;
 
+use std::sync::Arc;
+
+use super::{Components, Initialize};
 use crate::*;
-use modules::Components;
 
 pub struct Uptime(Instant);
 
@@ -12,15 +16,32 @@ impl Uptime {
     }
 }
 
-impl super::Initialize for Uptime {
+impl Initialize for Uptime {
     fn initialize(Components { commands, .. }: &mut Components<'_>) -> anyhow::Result<()> {
         let handle = |this: Arc<Self>, ctx: Context<CommandArgs>| async move {
             ctx.say(format!(
-                "I've been running for {}",
+                "I've been running for {}.",
                 this.0.elapsed().relative_time()
             ))
         };
 
         commands.command(Self::new(), "!uptime", handle)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use mock_instant::MockClock;
+    use std::time::Duration;
+
+    use super::*;
+    use crate::TestRunner;
+
+    #[test]
+    fn uptime() {
+        TestRunner::new("!uptime")
+            .say("I've been running for 1 minute and 1 second.")
+            .with_module(Uptime::initialize)
+            .run_commands(|| MockClock::advance(Duration::from_secs(61)))
     }
 }
